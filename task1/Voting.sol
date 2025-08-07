d94f4958b5d70a28840957522140b9022ffdf77a
wangyping <244536433@qq.com>
2025-08-07 11:17:45 +0800

    Update Voting.sol

--- task1/Voting.sol
+++ task1/Voting.sol
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+// ✅ 创建一个名为Voting的合约，包含以下功能：
+// 一个mapping来存储候选人的得票数
+// 一个vote函数，允许用户投票给某个候选人
+// 一个getVotes函数，返回某个候选人的得票数
+// 一个resetVotes函数，重置所有候选人的得票数
+
+contract Voting {
+    mapping(string candidate=> uint256 voteNum) public candidateMap;
+    event DebugUint(uint256 value);
+
+    constructor(){
+
+    }
 
     function vote(string memory candidate) public {
         candidateMap[candidate] += 1;
