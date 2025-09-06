aec436ff4a84349f93fa3cf7840040631609963e
wangyping <244536433@qq.com>
2025-09-06 16:20:29 +0800

    Update MyToken.sol

--- task2/MyToken.sol
+++ task2/MyToken.sol
@@ -1,1 +1,122 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+// 合约包含以下标准 ERC20 功能：
+// balanceOf：查询账户余额。
+// transfer：转账。
+// approve 和 transferFrom：授权和代扣转账。
+// 使用 event 记录转账和授权操作。
+// 提供 mint 函数，允许合约所有者增发代币。
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owr, address indexed spender, uint256 value);
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owr, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+contract MyToken is IERC20 {
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    
+    address public owner;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+    
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        owner = msg.sender;
+    }
+    
+    function name() public view returns (string memory) {
+        return _name;
+    }
+    
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+    
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+    
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+    
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+    
+    function transfer(address to, uint256 value) public override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function allowance(address owr, address spender) public view override returns (uint256) {
+        return _allowances[owr][spender];
+    }
+    
+    function approve(address spender, uint256 value) public override returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint256 value) public override returns (bool) {
+        uint256 currentAllowance = _allowances[from][msg.sender];
+        require(currentAllowance >= value, "ERC20: transfer amount exceeds allowance");
+        _transfer(from, to, value);
+        _approve(from, msg.sender, currentAllowance - value);
+        return true;
+    }
+    
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+    
+    function _transfer(address from, address to, uint256 value) internal {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= value, "ERC20: transfer amount exceeds balance");
+        
+        _balances[from] = fromBalance - value;
+        _balances[to] += value;
+        
+        emit Transfer(from, to, value);
+    }
+    
+    function _approve(address owr, address spender, uint256 value) internal {
+        require(owr != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        
+        _allowances[owr][spender] = value;
+        emit Approval(owr, spender, value);
+    }
+    
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+        
+        _totalSupply += amount;
+        _balances[account] += amount;
+        
+        emit Transfer(address(0), account, amount);
+    }
+}
